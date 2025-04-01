0ecee68cd5b11ab63c460af2863b0b200cc6d03f
Catalina Syddall <catalinasyddall@gmail.com>
2025-04-01 16:19:14 -0300

    adds context values to layour

--- src/Pages/Layout/index.tsx
+++ src/Pages/Layout/index.tsx
@@ -1,14 +1,18 @@
+import { useContext } from 'react';
 import Canvas from '../../Components/Canvas';
 import SettingNavbar from '../../Components/SettingNavbar';
 import { ContentContainer, Root } from './styles';
+import { Context } from '../../Context';
 
 const Layout = () => {
+  const { selectedDesign } = useContext(Context);
+
   return (
     <Root>
-      <h1>Tile Generator</h1>
+      <h1>Pattern Generator</h1>
       <ContentContainer>
         <SettingNavbar />
-        <Canvas />
+        <Canvas design={selectedDesign} />
       </ContentContainer>
     </Root>
   );
